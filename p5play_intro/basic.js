10e9901aa3cba27edcbc21be3a271b0921f1f049
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-13 04:30:01 +0000

    Jul 13, 2025, 4:30 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -16,4 +16,19 @@ function setup() {
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
 
+  createCanvas(600,400);
+  background(200);
 }
+
+function draw(){
+  fill(255,0,0); //rgb
+  ellipse(300,100,100,100); // x,axis, y-axis, size
+  fill(255,0,0); //rgb
+  ellipse(300,100,100,100); // x,axis, y-axis, size
+  fill(255,255,0); //rgb
+  ellipse(300,200,100,100); // x,axis, y-axis, size
+  fill(0,255,0); //rgb
+  ellipse(300,300,100,100); // x,axis, y-axis, size
+}
+
+
