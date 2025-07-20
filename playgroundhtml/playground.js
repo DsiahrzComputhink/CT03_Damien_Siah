c2dae6280e63b8f36a936e857c7b727becd8620d
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 12:19:29 +0000

    Jul 20, 2025, 12:19 PM

--- playgroundhtml/playground.js
+++ playgroundhtml/playground.js
@@ -67,6 +67,7 @@ function draw() {
   text("Current Speed: ("+ (int((currentvel/startingvelocity)*100)) + "%)", 10,80);
 
   // Or operator --> ||
+  // ball
   if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
     ball.vel.x *= -1;
   }
@@ -74,8 +75,8 @@ function draw() {
     ball.vel.y *= -1;
   }
 
-  if ( ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
-    ball.vel.y *= -1;
+  if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
+    ball.vel.x *= -1;
   }
 
   if(box.x == ball.x){
