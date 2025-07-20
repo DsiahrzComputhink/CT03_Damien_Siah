9d1feccd89c216d4de609f82cbaeb8a42901de9a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 12:11:24 +0000

    Jul 20, 2025, 12:11 PM

--- playgroundhtml/playground.js
+++ playgroundhtml/playground.js
@@ -52,7 +52,7 @@ function draw() {
 
   text("Mouse: (" + int(mouseX) + ", " + int(mouseY) + ")", 10,60);
 
-  text("Testing: ("+ currentvel + ")", 10,40);
+  text("Testing: ("+ currentvel + ")", 10,80);
 
   // Or operator --> ||
   if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
