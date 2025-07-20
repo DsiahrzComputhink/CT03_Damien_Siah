6202197f0f0ef524790392c816aa83d5a20419ac
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 04:06:00 +0000

    Jul 20, 2025, 4:06 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -43,7 +43,8 @@ function draw() {
 
       fill(0);
       textSize(16)
-      text("Ball Coordinates: (" + int(ball.x) + ", " + int(ball.y) + ")")
+      text("Ball Coordinates: (" + int(ball.x) + ", " + int(ball.y) + ")", 10,20);
+      text("Mouse:")
       // Or operator --> ||
     if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
       ball.vel.x *= -1;
