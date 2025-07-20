3fb41441bba51936a6731abc1f149e4b71cc3ecd
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 04:01:46 +0000

    Jul 20, 2025, 4:01 AM

--- playgroundhtml/playground.js
+++ playgroundhtml/playground.js
@@ -1,48 +1,52 @@
-// Archive
+//Anchored
 function setup() {
-  // Set up the canvas
-  new Canvas(800, 400); // top left is (0,0)
-  background(250); //background color
+    // Set up the canvas
+    new Canvas(800, 400); // top left is (0,0)
+    background(250); //background color
 
-  // Basic shape testing
-  // write your codes here
-  // End Basic shape testing
-  fill("skyblue");
-  stroke("pink");
-  strokeWeight(10);
-  
-  circle(30,30,50); //(x, y, d)
-  // (x,y) refers to the position in space
-  // d refers to the diameter of the object
+    // Basic shape testing
+    // write your codes here
 
-  rect(50,50,100,200) //(x, y, w, h)
-  // (x,y) refers to the position in space
-  // (w,h) refers to the width and height of the object
+    // fill("skyblue");
+    // stroke("pink");
+    // strokeWeight(10);
+    
+    // circle(30,30,50); //(x, y, d)
+    // rect(50,50,100,200) //(x, y, w, h)
 
-  
-  // Create a bouncing ball sprite
-  // write your codes here
-  ball = new Sprite();
-  // location of ball
-  ball.x = 100;
-  ball.y = 200;
-  ball.diameter = 40;
-  ball.color = "blue";
-  ball.vel.x = 3;
-  ball.vel.y = 3;
-  ball.bounciness = 1;
-  ball.collider = "dynamic"; // optional --> obeys the physics --> default
+    // End Basic shape testing
+    
+    // Create a bouncing ball sprite
+    // write your codes here
+    ball = new Sprite();
+    // location of ball
+    ball.x = 400;
+    ball.y = 200;
+    ball.diameter = 40;
+    ball.color = "blue";
+    ball.vel.x = 3;
+    ball.vel.y = 9;
+    ball.bounciness = 1;
+    ball.collider = "dynamic"; // optional --> obeys the physics --> default
 
+    // another sprite that is a rectangle
+    box = new Sprite();
+    box.x = 100;
+    box.y = 100;
+    box.w = 50;
+    box.h = 50;
+    box.color = "green";
+    box.collider = "static" // fixed but others can bounce off
 }
-
 function draw() {
+  // write your codes here
     background(250);
-        // Or operator --> ||
-    if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
-        ball.vel.x *= -1;
-    }
-    if ( ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
-        ball.vel.y *= -1;
-    }
-    }
+    // Or operator --> ||
+  if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
+    ball.vel.x *= -1;
+  }
+  if ( ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
+    ball.vel.y *= -1;
+  }
+}
 
