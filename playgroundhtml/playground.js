cd626ce2d61cf0b3ac13b55e0caf875f857600a6
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 12:37:24 +0000

    Jul 20, 2025, 12:37 PM

--- playgroundhtml/playground.js
+++ playgroundhtml/playground.js
@@ -69,10 +69,10 @@ function draw() {
   // Or operator --> ||
   // ball
   if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
-    ball.vel.x *= -1.1;
+    ball.vel.x *= -1;
   }
   if ( ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
-    ball.vel.y *= -1.1;
+    ball.vel.y *= -1;
   }
 
   // square
@@ -101,3 +101,28 @@ function draw() {
   box.y = mouseY;
 }
 
+
+
+import { engine, animate, utils } from 'animejs';
+
+const [ $container ] = utils.$('.container');
+const [ $range ] = utils.$('.range');
+
+for (let i = 0; i < 150; i++) {
+  const $particle = document.createElement('div');
+  $particle.classList.add('particle');
+  $container.appendChild($particle);
+  animate($particle, {
+    x: utils.random(-10, 10, 2) + 'rem',
+    y: utils.random(-3, 3, 2) + 'rem',
+    scale: [{ from: 0, to: 1 }, { to: 0 }],
+    delay: utils.random(0, 1000),
+    loop: true,
+  });  
+}
+
+function onInput() {
+  utils.sync(() => engine.speed = this.value);
+}
+
+$range.addEventListener('input', onInput);
