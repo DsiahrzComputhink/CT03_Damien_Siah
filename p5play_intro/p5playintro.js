bbf821eca2c1257f3ba16895d2648a866c7e47b1
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 04:11:19 +0000

    Jul 20, 2025, 4:11 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -53,7 +53,7 @@ function draw() {
       ball.vel.y *= -1;
     }
 
-    if ( ball.x < 0 + box.width || ball.x > width - ball.diameter / 2){
+    if ( box.y  || ball.x > width - ball.diameter / 2){
       ball.vel.x *= -1;
     }
     if ( ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
