098fda8aeb36f709ab3802916339067208866877
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 03:37:53 +0000

    Jul 20, 2025, 3:37 AM

--- playgroundhtml/playground.js
+++ playgroundhtml/playground.js
@@ -1,1 +1,35 @@
 // Archive
+function setup() {
+  // Set up the canvas
+  new Canvas(800, 400); // top left is (0,0)
+  background(250); //background color
+
+  // Basic shape testing
+  // write your codes here
+  // End Basic shape testing
+  fill("skyblue");
+  stroke("pink");
+  strokeWeight(10);
+  
+  circle(30,30,50); //(x, y, d)
+  rect(50,50,100,200) //(x, y, w, h)
+  
+  // Create a bouncing ball sprite
+  // write your codes here
+  ball = new Sprite();
+  // location of ball
+  ball.x = 100;
+  ball.y = 200;
+  ball.diameter = 40;
+  ball.color = "blue";
+  ball.vel.x = 3;
+  ball.vel.y = 3;
+  ball.bounciness = 1;
+  ball.collider = "dynamic"; // optional --> obeys the physics --> default
+
+}
+
+function draw() {
+  // write your codes here
+}
+
