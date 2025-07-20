043d6677e6d5799668b82ed241e2f71323830f5a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 04:01:38 +0000

    Jul 20, 2025, 4:01 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,41 +1,41 @@
 function setup() {
-  // Set up the canvas
-  new Canvas(800, 400); // top left is (0,0)
-  background(250); //background color
+    // Set up the canvas
+    new Canvas(800, 400); // top left is (0,0)
+    background(250); //background color
 
-  // Basic shape testing
-  // write your codes here
+    // Basic shape testing
+    // write your codes here
 
-  // fill("skyblue");
-  // stroke("pink");
-  // strokeWeight(10);
-  
-  // circle(30,30,50); //(x, y, d)
-  // rect(50,50,100,200) //(x, y, w, h)
+    // fill("skyblue");
+    // stroke("pink");
+    // strokeWeight(10);
+    
+    // circle(30,30,50); //(x, y, d)
+    // rect(50,50,100,200) //(x, y, w, h)
 
-  // End Basic shape testing
-  
-  // Create a bouncing ball sprite
-  // write your codes here
-  ball = new Sprite();
-  // location of ball
-  ball.x = 400;
-  ball.y = 200;
-  ball.diameter = 40;
-  ball.color = "blue";
-  ball.vel.x = 3;
-  ball.vel.y = 9;
-  ball.bounciness = 1;
-  ball.collider = "dynamic"; // optional --> obeys the physics --> default
+    // End Basic shape testing
+    
+    // Create a bouncing ball sprite
+    // write your codes here
+    ball = new Sprite();
+    // location of ball
+    ball.x = 400;
+    ball.y = 200;
+    ball.diameter = 40;
+    ball.color = "blue";
+    ball.vel.x = 3;
+    ball.vel.y = 9;
+    ball.bounciness = 1;
+    ball.collider = "dynamic"; // optional --> obeys the physics --> default
 
-  // another sprite that is a rectangle
-  box = new Sprite();
-  box.x = 100;
-  box.y = 100;
-  box.w = 50;
-  box.h = 50;
-  box.color = "green";
-  box.collider = "static" // fixed but others can bounce off
+    // another sprite that is a rectangle
+    box = new Sprite();
+    box.x = 100;
+    box.y = 100;
+    box.w = 50;
+    box.h = 50;
+    box.color = "green";
+    box.collider = "static" // fixed but others can bounce off
 }
 function draw() {
   // write your codes here
