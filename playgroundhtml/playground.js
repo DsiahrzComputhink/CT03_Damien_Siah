1922190dc3ff3e7462736ac923ef8f011a74e825
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 04:00:50 +0000

    Jul 20, 2025, 4:00 AM

--- playgroundhtml/playground.js
+++ playgroundhtml/playground.js
@@ -36,13 +36,13 @@ function setup() {
 }
 
 function draw() {
-  background(250);
-    // Or operator --> ||
-  if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
-    ball.vel.x *= -1;
-  }
-  if ( ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
-    ball.vel.y *= -1;
-  }
-}
+    background(250);
+        // Or operator --> ||
+    if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
+        ball.vel.x *= -1;
+    }
+    if ( ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
+        ball.vel.y *= -1;
+    }
+    }
 
