1763d741700f83f95d34ba4e8547b318a99c685b
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 04:02:44 +0000

    Jul 20, 2025, 4:02 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -36,6 +36,8 @@ function setup() {
     box.h = 50;
     box.color = "green";
     box.collider = "static" // fixed but others can bounce off
+    box.x = MouseX;
+    box.y = MouseY;
 }
 function draw() {
   // write your codes here
