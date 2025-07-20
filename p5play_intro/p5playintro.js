329f911312880d949a00ba81b435bae1581aba21
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 04:30:31 +0000

    Jul 20, 2025, 4:30 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -46,6 +46,6 @@ function draw() {
   ellipse(200,100,80,150);
   circle(320,120,100);
   rect(30,20,55,50,0,0,20,0); // (x, y, width, height, [tl], [tr], [bl], [br])
-  triangle(30,75,58,20,86,75) // (x1, y1, x2, y2, x3, y3) [Plots 3 points an]
+  triangle(30,75,58,20,86,75) // (x1, y1, x2, y2, x3, y3) [Plots 3 points and creates a triangle]
 }
 
