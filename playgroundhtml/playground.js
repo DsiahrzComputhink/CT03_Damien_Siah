855a08c5ef939ff295981ca2ed087a98a6a58cb5
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-20 12:00:01 +0000

    Jul 20, 2025, 12:00 PM

--- playgroundhtml/playground.js
+++ playgroundhtml/playground.js
@@ -45,8 +45,9 @@ function draw() {
       fill(0);
       textSize(16)
       text("Ball: (" + int(ball.x) + ", " + int(ball.y) + ")", 10,20);
-      text("Velocity: ("+ (Math.abs(ball.vel.x) + Math.abs(ball.vel.y)) + ")", 10,40);
+      text("Ball Velocity: ("+ (Math.abs(ball.vel.x) + Math.abs(ball.vel.y)) + ")", 10,40);
       text("Mouse: (" + int(mouseX) + ", " + int(mouseY) + ")", 10,60);
+      text("Velocity: ("+ (Math.abs(ball.vel.x) + Math.abs(ball.vel.y)) + ")", 10,40);
       // Or operator --> ||
     if ( ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
       ball.vel.x *= -1;
