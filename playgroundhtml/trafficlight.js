72d2c87229dc9f0832b635b6abea3d97c0c4bbfc
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-27 04:13:59 +0000

    Jul 27, 2025, 4:13 AM

---
+++ playgroundhtml/trafficlight.js
@@ -0,0 +1,33 @@
+function setup() {
+  // write your codes here
+
+  // --- Exercise: Area of Triangle ---
+  // write your codes here
+
+  // --- Exercise: Sum of first 10 even numbers ---
+  // write your codes here
+
+  // --- Exercise: Age category classification ---
+  // write your codes here
+
+  // --- Exercise: Display odd numbers backward using while loop ---
+  // write your codes here
+
+  // --- Exercise: Array operations (groceries) ---
+  // write your codes here
+
+  createCanvas(600,400);
+  background(200);
+}
+
+function draw(){
+  fill(255,0,0); //rgb
+  ellipse(300,100,100,100); // x,axis, y-axis, size
+  fill(255,0,0); //rgb
+  ellipse(300,100,100,100); // x,axis, y-axis, size
+  fill(255,255,0); //rgb
+  ellipse(300,200,100,100); // x,axis, y-axis, size
+  fill(0,255,0); //rgb
+  ellipse(300,300,100,100); // x,axis, y-axis, size
+}
+
