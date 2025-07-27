035781f248f0df815599efba8403c13cf34c5e83
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-27 03:07:27 +0000

    Jul 27, 2025, 3:07 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -50,6 +50,7 @@ function draw() {
   fill(0,255,255); // rgb
   circle(320,120,100);
   fill(90,255,0); // rgb
+  stroke()
   rect(30,20,55,50,0,0,20,0); // (x, y, width, height, [tl], [tr], [bl], [br])
   triangle(30,75,58,20,86,75) // (x1, y1, x2, y2, x3, y3) [Plots 3 points and creates a triangle]
   quad(65,90,140,300,400,500,90,150) // (x1, y1, x2, y2, x3, y3) [Plots 4 points and creates a polygon]
