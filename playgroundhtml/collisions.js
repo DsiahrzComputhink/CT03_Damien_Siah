d66652176ed73b8a0e040049c62fb0730fab2e0e
DsiahrzComputhink <damiensiah11@gmail.com>
2025-07-29 02:46:49 +0000

    Jul 29, 2025, 2:46 AM

--- playgroundhtml/collisions.js
+++ playgroundhtml/collisions.js
@@ -104,7 +104,7 @@ function draw() {
     ball.vel.y *= startingvelocity/ballcurrentvel
   }
 
-  if(mouseX < Canvas.x)
+  if(mouseX < Canvas.x || mouseY < height)
   box.x = mouseX;
   box.y = mouseY;
 }
