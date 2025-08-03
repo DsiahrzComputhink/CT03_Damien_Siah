c129eb546cb1ca77878c2bb907c03bfcebd1c373
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-03 03:29:06 +0000

    Aug 3, 2025, 3:29 AM

--- playgroundhtml/collisions.js
+++ playgroundhtml/collisions.js
@@ -117,10 +117,10 @@ function draw() {
 
 
   // to fix clipping
-  if ( ball.x < (0 - 100) + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
+  if ( ball.x < (0 - 10) + ball.diameter / 2 || ball.x > (width + 10) - ball.diameter / 2){
     ball.vel.x *= -1;
   }
-  if ( ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
+  if ( ball.y < (0 - 10) + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
     ball.vel.y *= -1;
   }
 
