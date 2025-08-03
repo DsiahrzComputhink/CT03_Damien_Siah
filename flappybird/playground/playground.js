93bfb5ae89f573f689a6660435925887f6ca58e7
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-03 03:52:37 +0000

    Aug 3, 2025, 3:52 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -4,6 +4,6 @@ let flapMidImg, bg, base;
 function preload(){
   flapMidImg = loadImage('../assets/yellowbird-midflap.png')
   bg =loadImage('../assets/background-day.png')
-  base = loadImage('../assets/.png')
-  
+  base = loadImage('../assets/base.png')
 }
+
