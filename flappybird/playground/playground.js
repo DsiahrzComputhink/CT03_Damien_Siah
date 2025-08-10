bdb5bc1f363825e18fa12df3df1551da3dd2d516
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-10 04:30:01 +0000

    Aug 10, 2025, 4:30 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -59,30 +59,30 @@ function draw(){
 
   // Smooth Rotation
 
-  if (bird.vel.y < -1){
-    bird.img = flapUpImg;
-    if(bird.rotation > (-20)){
-      bird.rotation -= 4;
-    }
-  }
-  else if(bird.vel.y > 1 ){
-    bird.img = flapDownImg;
-    if(bird.rotation < 20){
-      bird.rotation += 2;
-    }
-  }
-  else{
-    bird.img = flapMidImg;
-    if(bird.rotation > 0){
-      bird.rotation -= 1;
-    }
-    else if(bird.rotation < 0){
-      bird.rotation += 1;
-    }
-    else{
-      bird.rotation = 0;
-    }
-  }
+  // if (bird.vel.y < -1){
+  //   bird.img = flapUpImg;
+  //   if(bird.rotation > (-20)){
+  //     bird.rotation -= 4;
+  //   }
+  // }
+  // else if(bird.vel.y > 1 ){
+  //   bird.img = flapDownImg;
+  //   if(bird.rotation < 20){
+  //     bird.rotation += 2;
+  //   }
+  // }
+  // else{
+  //   bird.img = flapMidImg;
+  //   if(bird.rotation > 0){
+  //     bird.rotation -= 1;
+  //   }
+  //   else if(bird.rotation < 0){
+  //     bird.rotation += 1;
+  //   }
+  //   else{
+  //     bird.rotation = 0;
+  //   }
+  // }
 
   fill("black");
   textSize(15);
