4413f06f072aeeb14808285f37ec70c6c1f2ef2b
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-10 04:31:27 +0000

    Aug 10, 2025, 4:31 AM

--- flappybird/playground/pg05a_spriteanimation.js
+++ flappybird/playground/pg05a_spriteanimation.js
@@ -1,24 +1,92 @@
-  if (bird.vel.y < -1){
+let bird,floor;
+let flapMidImg, bg, base;
+
+function preload(){
+  flapMidImg = loadImage('assets/yellowbird-midflap.png');
+  bg =loadImage('assets/background-day.png');
+  base = loadImage('assets/base.png');
+  flapUpImg = loadImage('assets/bluebird-upflap.png');
+  flapDownImg = loadImage('assets/redbird-downflap.png');
+}
+
+function setup(){
+  new Canvas(400,600);
+
+  // bird sprite, location, size
+  bird = new Sprite();
+  bird.x = width / 2;
+  bird.y = 500;
+  bird.width = 30;
+  bird.height = 30;
+  bird.img = flapMidImg;
+
+  bird.collider = 'dynamic';
+  bird.mass = 2;
+  bird.drag = 0.02;
+  bird.bounciness = 0.99;
+  world.gravity.y = 10;
+
+  floor = new Sprite();
+  floor.x = 200;
+  floor.y = height - 20;
+  floor.width = 400;
+  floor.height = 125;
+  floor.collider = "static";
+  floor.img = base;
+}
+
+function draw(){
+  image(bg, 0, 0, width, height);
+
+  // keybinds
+  if(kb.presses('space') || mouse.presses()){
+    bird.vel.y = -5;
+    bird.sleeping = false;
+  }
+
+    if (bird.vel.y < -1){
     bird.img = flapUpImg;
-    if(bird.rotation > (-20)){
-      bird.rotation -= 4;
-    }
+    bird.rotation = -30
   }
   else if(bird.vel.y > 1 ){
     bird.img = flapDownImg;
-    if(bird.rotation < 20){
-      bird.rotation += 2;
-    }
+    bird.rotation = 30
   }
   else{
     bird.img = flapMidImg;
-    if(bird.rotation > 0){
-      bird.rotation -= 1;
-    }
-    else if(bird.rotation < 0){
-      bird.rotation += 1;
-    }
-    else{
-      bird.rotation = 0;
-    }
+    bird.rotation = 0
   }
+
+  // Smooth Rotation
+
+  // if (bird.vel.y < -1){
+  //   bird.img = flapUpImg;
+  //   if(bird.rotation > (-20)){
+  //     bird.rotation -= 4;
+  //   }
+  // }
+  // else if(bird.vel.y > 1 ){
+  //   bird.img = flapDownImg;
+  //   if(bird.rotation < 20){
+  //     bird.rotation += 2;
+  //   }
+  // }
+  // else{
+  //   bird.img = flapMidImg;
+  //   if(bird.rotation > 0){
+  //     bird.rotation -= 1;
+  //   }
+  //   else if(bird.rotation < 0){
+  //     bird.rotation += 1;
+  //   }
+  //   else{
+  //     bird.rotation = 0;
+  //   }
+  // }
+
+  fill("black");
+  textSize(15);
+  text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
+  text('isMoving' + bird.isMoving, 10, 40);
+  text('sleeping' + bird.sleeping, 10, 60);
+}
