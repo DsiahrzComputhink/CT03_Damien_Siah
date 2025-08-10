7ae73424800e334fc7b6c24d873f111851bf2099
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-10 04:11:04 +0000

    Aug 10, 2025, 4:11 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -52,6 +52,9 @@ function draw(){
   }
   else if(bird.vel.y > 1 ){
     bird.img = flapDownImg;
+    if(bird.vel.y > -1){
+      bird.rotation = -30 * bird.vel.y
+    }
   }
   else{
     bird.img = flapMidImg;
