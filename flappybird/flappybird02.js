12ee0f1ee73724c480ad6ece6ebcf9dd49930ea6
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-10 03:45:25 +0000

    Aug 10, 2025, 3:45 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -5,7 +5,7 @@ function preload(){
   flapMidImg = loadImage('assets/yellowbird-midflap.png');
   bg =loadImage('assets/background-day.png');
   base = loadImage('assets/base.png');
-  flapUpImg = loadImGE
+  flapUpImg = loadImage('assets')
 }
 
 function setup(){
