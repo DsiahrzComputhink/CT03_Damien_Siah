e4863ff4574886314550cd53a109511bd38c4a48
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-10 04:00:24 +0000

    Aug 10, 2025, 4:00 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -23,7 +23,7 @@ function setup(){
   bird.collider = 'dynamic';
   bird.mass = 2;
   bird.drag = 0.02;
-  bird.bounciness = 0.5;
+  bird.bounciness = 0.999;
   world.gravity.y = 10;
 
   floor = new Sprite();
