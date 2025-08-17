35489b52b57a2d0e1c8de8e488ca706894bf42a8
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-17 04:18:01 +0000

    Aug 17, 2025, 4:18 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -92,11 +92,11 @@ function spawnPipePair(Xposition) {
   let midY = random(250, height - 250); // random(min, max)
 
   // new sprite
-  bottomPipe = new Sprite(Xposition + 400, midY + gap/2 + 200, 52, 320, "static");
+  bottomPipe = new Sprite(Xposition, midY + gap/2 + 200, 52, 320, "static");
   bottomPipe.img = pipeImg;
 
   // new sprite
-  topPipe = new Sprite(Xposition + 400, midY - gap/2 - 200, 52, 320, "static");
+  topPipe = new Sprite(Xposition, midY - gap/2 - 200, 52, 320, "static");
   topPipe.img = pipeImg;
   topPipe.rotation = 180;
 
