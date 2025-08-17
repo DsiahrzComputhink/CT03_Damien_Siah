bdc956a876658cb71ce8ac748b9b1764d4c72569
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-17 03:53:03 +0000

    Aug 17, 2025, 3:53 AM

--- playgroundhtml/flappybirdarchive05.js
+++ playgroundhtml/flappybirdarchive05.js
@@ -34,10 +34,6 @@ function setup(){
   floor.collider = "static";
   floor.img = base;
 
-
-  line1 = new Sprite(350, height/2 + gap/2 + 200, 52, 320, "static");
-
-  line2 = new Sprite(50, height/2 + gap/2 + 200, 52, 320, "static");
 }
 
 function draw(){
