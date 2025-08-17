0be4fba7fda568436d3b2f52c9a7479fc07efd84
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-17 03:34:55 +0000

    Aug 17, 2025, 3:34 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -82,8 +82,8 @@ function draw(){
 function spawnPipePair() {
   let gap = 50;
   let midY = height/2;
-  
-  bottomPipe = new Sprite(100, height/2, 52, 320, "static");
+
+  bottomPipe = new Sprite(100, height/2 + 200, 52, 320, "static");
   bottomPipe.img = pipeImg;
 
   pipeGroup.add(bottomPipe);
