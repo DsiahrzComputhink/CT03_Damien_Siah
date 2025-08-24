7cc726b80eefd59d988d51dbeb14208bb3cbd58c
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-24 03:22:20 +0000

    Aug 24, 2025, 3:22 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -80,7 +80,7 @@ function draw(){
   }
 
   if (bird.collides(pipeGroup) || bird.collides(floor) ) {
-    newsprite = new Sprite(200, Canvas., 52, 320, "static");
+    newsprite = new Sprite(Canvas.x / 2, Canvas.y / 2, 52, 320, "static");
     noLoop();
   }
 
