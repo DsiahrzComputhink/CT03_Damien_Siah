4bdf8b2f0204c7a42a2cc390bd0a6f809068f61a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-24 03:11:01 +0000

    Aug 24, 2025, 3:11 AM

--- playgroundhtml/flappybirdarchive06.js
+++ playgroundhtml/flappybirdarchive06.js
@@ -1,12 +1,23 @@
 let bird,floor;
 let flapMidImg, bg, base;
 
+let flapUpImg, flapDownImg; // flap image
+
+let pipeImg; // Pipes
+let pipeGroup; // Organisation
+let bottomPipe, topPipe;
+let pipeSpace = 50; // Distance between each pipe
+
+let lastPipePosition; // debug
+
+
 function preload(){
   flapMidImg = loadImage('assets/yellowbird-midflap.png');
   bg =loadImage('assets/background-day.png');
   base = loadImage('assets/base.png');
   flapUpImg = loadImage('assets/bluebird-upflap.png');
   flapDownImg = loadImage('assets/redbird-downflap.png');
+  pipeImg = loadImage('assets/pipe-green.png');
 }
 
 function setup(){
@@ -23,17 +34,12 @@ function setup(){
   bird.collider = 'dynamic';
   bird.mass = 2;
   bird.drag = 0.02;
-  bird.bounciness = 0.9;
+  bird.bounciness = 0.99;
   world.gravity.y = 10;
 
-  floor = new Sprite();
-  floor.x = 200;
-  floor.y = height - 20;
-  floor.width = 400;
-  floor.height = 125;
-  floor.collider = "static";
-  floor.img = base;
+  CreateFloor(0 - 399);
 
+  pipeGroup = new Group();
 }
 
 function draw(){
@@ -45,56 +51,80 @@ function draw(){
     bird.sleeping = false;
   }
 
-  if(kb.presses('enter')){
-    square = new Sprite(mouse.x, mouse.y, 30, 30,'dynamic');
-  }
-
-
-  // Default Rotation
-
-  // if (bird.vel.y < -1){
-  //   bird.img = flapUpImg;
-  //   bird.rotation = -30
-  // }
-  // else if(bird.vel.y > 1 ){
-  //   bird.img = flapDownImg;
-  //   bird.rotation = 30
-  // }
-  // else{
-  //   bird.img = flapMidImg;
-  //   bird.rotation = 0
-  // }
-
-  // Smooth Rotation
-
+  // flap sequence
   if (bird.vel.y < -1){
     bird.img = flapUpImg;
-    if(bird.rotation > (-20)){
-      bird.rotation -= 4;
-    }
+    bird.rotation = -30
   }
   else if(bird.vel.y > 1 ){
     bird.img = flapDownImg;
-    if(bird.rotation < 20){
-      bird.rotation += 2;
-    }
+    bird.rotation = 30
   }
   else{
     bird.img = flapMidImg;
-    if(bird.rotation > 0){
-      bird.rotation -= 1;
-    }
-    else if(bird.rotation < 0){
-      bird.rotation += 1;
-    }
-    else{
-      bird.rotation = 0;
-    }
+    bird.rotation = 0
   }
+
+  // bird movement
+  bird.x = bird.x + 3;
+  camera.x = bird.x;
+
+  if (bird.x % 399 > 100) {
+    CreateFloor(bird.x - (bird.x % 399));
+  }
+
+  if (bird.x % pipeSpace === 0) {
+    spawnPipePair(bird.x - (bird.x % pipeSpace));
+  }
+
+  if (bird.collides(pipeGroup) || bird.collides(floor) ) {
+    noLoop();
+  }
+
+  // if (frameCount === 1) {
+  //   spawnPipePair(bird.x + 400); // custom function
+  // }
+
+
 
   fill("black");
   textSize(15);
   text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
   text('isMoving' + bird.isMoving, 10, 40);
   text('sleeping' + bird.sleeping, 10, 60);
+  text('bird.x: ' + bird.x.toFixed(2), 10, 80);
+}
+
+function spawnPipePair(Xposition) {
+  let gap = 75;
+  let midY = random(250, height - 250); // random(min, max)
+
+
+  console.log(Xposition - lastPipePosition)
+  lastPipePosition = Xposition
+
+  // new sprite
+  bottomPipe = new Sprite(400 + Xposition, midY + gap/2 + 200, 52, 320, "static");
+  bottomPipe.img = pipeImg;
+
+  // new sprite
+  topPipe = new Sprite(400 + Xposition, midY - gap/2 - 200, 52, 320, "static");
+  topPipe.img = pipeImg;
+  topPipe.rotation = 180;
+
+  pipeGroup.add(topPipe);
+
+  pipeGroup.add(bottomPipe);
+  pipeGroup.layer = 0; // backmost layer
+
+}
+
+function CreateFloor(Xposition) {
+  floor = new Sprite();
+  floor.x = Xposition + 500;
+  floor.y = height - 20;
+  floor.width = 399;
+  floor.height = 125;
+  floor.collider = "static";
+  floor.img = base;
 }
