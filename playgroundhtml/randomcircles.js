b162feaae63b582ccfad9dc8c22ad0780d793f8f
DsiahrzComputhink <damiensiah11@gmail.com>
2025-08-31 04:29:06 +0000

    Aug 31, 2025, 4:29 AM

--- playgroundhtml/randomcircles.js
+++ playgroundhtml/randomcircles.js
@@ -2,10 +2,15 @@ function setup(){
   new Canvas(400,400);
 }
 
+let objects = [];
 function draw(){
     if(kb.presses('space') || mouse.presses()){
       for (let i = 0; i < 5; i++){
-          
+        
       }
   }
+}
+
+function randomcircles(){
+
 }
