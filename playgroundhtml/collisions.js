02acd33371eac09bf06fab3eef78420eda386aa6
DsiahrzComputhink <damiensiah11@gmail.com>
2025-09-07 03:35:20 +0000

    Sep 7, 2025, 3:35 AM

--- playgroundhtml/collisions.js
+++ playgroundhtml/collisions.js
@@ -63,6 +63,7 @@ function draw() {
   text("Ball Velocity: ("+ int(ballcurrentvel) + ")", 10,40);
 
   text("Box: (" + int(box.x) + ", " + int(box.y) + ")", 10,60);
+  let boxcurren
 
   text("Total Momentum: ("+ (int((ballcurrentvel/startingvelocity)*100)) + "%)", 10,80);
 
