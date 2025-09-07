8d2dd30830ada592c5da2af4e30662832b418924
DsiahrzComputhink <damiensiah11@gmail.com>
2025-09-07 03:39:31 +0000

    Sep 7, 2025, 3:39 AM

--- playgroundhtml/collisions.js
+++ playgroundhtml/collisions.js
@@ -65,7 +65,9 @@ function draw() {
   text("Box: (" + int(box.x) + ", " + int(box.y) + ")", 10,60);
   let boxcurrentvel = (Math.abs(box.vel.x) + Math.abs(ball.vel.y));
 
-  text("Total Energy:")+ (int((ballcurrentvel + boxcurrentvel/startingvelocity)*100)) + "%)", 10,100);
+  text("Total Energy: ("+ (int((ballcurrentvel + boxcurrentvel/startingvelocity)*100)) + "%)", 10,100);
+  text("Total Energy: ("+ (int((ballcurrentvel + boxcurrentvel/startingvelocity)*100)) + "%)", 10,100);
+  text("Total Energy: ("+ (int((ballcurrentvel + boxcurrentvel/startingvelocity)*100)) + "%)", 10,100);
 
 
   text("Square: (" + int(square.x) + ", " + int(square.y) + ")", 10,360);
