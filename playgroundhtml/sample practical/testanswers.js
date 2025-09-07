cf94a7b5494ce2c600cd67da943cf2e931f85b17
DsiahrzComputhink <damiensiah11@gmail.com>
2025-09-07 03:40:47 +0000

    Sep 7, 2025, 3:40 AM

--- playgroundhtml/sample practical/testanswers.js
+++ playgroundhtml/sample practical/testanswers.js
@@ -7,7 +7,7 @@ function setup() {
     new Canvas(600, 400);
     background("blue");
 
-    floor = new Sprite(width / 2, height, width, 50)
+    floor = new Sprite(width / 2, height, width, 50, "none");
 
     let abc = new Sprite()
     // abc.diameter = 50;
