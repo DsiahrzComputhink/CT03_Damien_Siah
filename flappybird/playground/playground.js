8f8d155d368822c3f6704b988c32628af9037d13
DsiahrzComputhink <damiensiah11@gmail.com>
2025-09-14 03:41:13 +0000

    Sep 14, 2025, 3:41 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -184,6 +184,6 @@ function drawScore(x, y, score, digitWidth, digitHeight){
   for( let i = 0; i < scoreStr.length; i++){
     let digit = int(scoreStr[i])
     let xPos = startX + i * digitWidth
-    let digitSprite = new scoreDigits.Sprite(xPos)
+    let digitSprite = new scoreDigits.Sprite(xPos, y, digitWidth, digitHeight);
   }
 }
