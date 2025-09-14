3673b40f2a1be5a0ced499b444c9a742f404ed49
DsiahrzComputhink <damiensiah11@gmail.com>
2025-09-14 03:55:35 +0000

    Sep 14, 2025, 3:55 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -198,5 +198,5 @@ function drawScore(x, y, score, digitWidth, digitHeight){
 
 function moveGroup(group, targetX, spacing){
   let totalWidth = (group.length - 1) * spacing;
-  
+  let startX = targetX - totalWidth 
 }
