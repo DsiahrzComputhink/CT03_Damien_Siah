f8e097739a3cf9c9f580b2f0332b46cd57e64159
DsiahrzComputhink <damiensiah11@gmail.com>
2025-09-28 04:07:47 +0000

    Sep 28, 2025, 4:07 AM

--- playgroundhtml/sample practical/testanswers.js
+++ playgroundhtml/sample practical/testanswers.js
@@ -14,7 +14,7 @@ function setup() {
     // abc.diameter = 50;
     // abc.y = 50;
 
-    world.gravity.y = 1; // speed of dropping
+    world.gravity.y = 10; // speed of dropping
 }
 
 function draw() {
