a13646dd4880fa132e50521acfa118880aad3959
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-05 08:08:07 +0000

    Oct 5, 2025, 8:08 AM

--- playgroundhtml/algoodo.js
+++ playgroundhtml/algoodo.js
@@ -18,6 +18,7 @@ function setup() {
 
     // Draw the right rectangle.
     Menu = new Sprite(width / 2, height / 2, width, height, "static");
+    Menu.setCollider("none"); // disables all collisions for this sprite
     Menu.color = color('rgba(0, 0, 0, 0.5)');
 
 }
