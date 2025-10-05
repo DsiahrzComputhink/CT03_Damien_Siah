bf2f3eeaeeb255376498ea4a32c6fe69a7cef1e2
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-05 04:17:21 +0000

    Oct 5, 2025, 4:17 AM

--- ml5/handpose_balloon.js
+++ ml5/handpose_balloon.js
@@ -59,7 +59,7 @@ function draw(){
 
     if (hands.length > 0) {
         let hand = hands[0]
-        let keypoint = handPose.keypoints[8]
+        let keypoint = hand.keypoints[8]
         circle(keypoint.x, keypoint.y, 30);
     }
 }
