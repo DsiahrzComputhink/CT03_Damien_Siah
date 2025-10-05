db6bfe38ba5c7e74e2a99a64f152c296f4aa444a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-05 04:00:06 +0000

    Oct 5, 2025, 4:00 AM

---
+++ ml5/handpose_basic copy.js
@@ -0,0 +1,63 @@
+let handPose;
+let VideoW = 640;
+let VideoH = 480;
+let video;
+let hands = [];
+
+function preload(){
+    let options = {
+        flipped: true,
+        runtime: 'tfjs',
+        modelType: "full",
+        detectorModelUrl: undefined,
+        landmarkModelUrl: undefined,
+    }
+
+    // loading the handpose model
+    handPose = ml5.handPose(options);
+}
+
+function setup(){
+    createCanvas(VideoW, VideoH)
+
+    let constraints = {
+        video: {
+            mandatory: {
+                minWidth: VideoW,
+                minHeight: VideoH
+            },
+
+            optional: [{minFrameRate: 60}]
+        },
+
+        audio: false,
+        flipped: true,
+    };
+
+    // Create the webcam video and hide it
+    video = createCapture(constraints);
+    video.size(640,480);
+    video.hide();
+
+    handPose.detectStart(video, gotHands);
+
+}
+
+function draw(){
+    image(video, 0, 0, VideoW, VideoH);
+
+    for(let i = 0; i < hands.length; i++){
+        let hand = hands[i]; // left or right
+
+        for(let j = 0 ; j < hand.keypoints.length; j++){
+            let keypoint = hand.keypoints[j];
+
+            circle(keypoint.x, keypoint.y, 10)
+        }
+
+    }
+}
+
+function gotHands(results){
+    hands = results;
+}
