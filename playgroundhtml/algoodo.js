a0a3dd1b0326b50f95ddcb429649848250b5fbb9
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-05 03:51:08 +0000

    Oct 5, 2025, 3:51 AM

--- playgroundhtml/algoodo.js
+++ playgroundhtml/algoodo.js
@@ -27,12 +27,10 @@ function draw() {
     text("R - Circle", 10,20);
     text("T - Ellipse", 10,35);
     text("Y - Square", 10,50);
-    text("U - Rectangle", 10,65);
-    text("I - Triangle", 10,80);
 
 
     // Sprite Creation
-    if ( keyboard.presses("R") || keyboard.presses("t") || keyboard.presses("y") || keyboard.presses("u") || keyboard.presses("i")) {
+    if ( keyboard.presses("R") || keyboard.presses("t") || keyboard.presses("y")) {
 
         if ( keyboard.presses("r")) {
             let s = new Sprite( mouseX, mouseY ) // Sphere
