244b6bcfded57dc63f0c8529e067436b43b3624a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-05 04:09:10 +0000

    Oct 5, 2025, 4:09 AM

--- ml5/handpose_balloon.js
+++ ml5/handpose_balloon.js
@@ -46,15 +46,20 @@ function setup(){
 function draw(){
     image(video, 0, 0, VideoW, VideoH);
 
-    for(let i = 0; i < hands.length; i++){
-        let hand = hands[i]; // left or right
+    // for(let i = 0; i < hands.length; i++){
+    //     let hand = hands[i]; // left or right
 
-        for(let j = 0 ; j < hand.keypoints.length; j++){
-            let keypoint = hand.keypoints[j];
+    //     for(let j = 0 ; j < hand.keypoints.length; j++){
+    //         let keypoint = hand.keypoints[j];
 
-            circle(keypoint.x, keypoint.y, 10)
-        }
+    //         circle(keypoint.x, keypoint.y, 10)
+    //     }
 
+    // }
+
+    if (hands.length > 0{
+        let keypoint = handPose.keypoints[8]
+        circle(keypoint.x, keypoint.y, 30);
     }
 }
 
