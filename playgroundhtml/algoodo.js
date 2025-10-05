f8cbf3cda3da6b549eee7feb635f01c51e0d0a50
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-05 08:49:11 +0000

    Oct 5, 2025, 8:49 AM

--- playgroundhtml/algoodo.js
+++ playgroundhtml/algoodo.js
@@ -43,8 +43,8 @@ function draw() {
     text("Y - Rectangle", 10,65);
 
     text("'-' or '+' to Adjust Size",10,85)
-    text("'A' or 'D' to Adjust Elasticity",10,100)
-    text("'A' or '' to Adjust Elasticity",10,100)
+    text("'A' or 'S' to Adjust Elasticity",10,100)
+    text("'Z' or 'X' to Adjust Gravity",10,100)
 
     text("Constants",600, 20)
     text("Size - " + size,600,35)
