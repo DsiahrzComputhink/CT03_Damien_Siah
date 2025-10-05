0e3c73ab24974f632034a1a640c4977fbfb7a3e3
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-05 08:58:52 +0000

    Oct 5, 2025, 8:58 AM

--- playgroundhtml/algoodo.js
+++ playgroundhtml/algoodo.js
@@ -46,10 +46,10 @@ function draw() {
     text("'A' or 'S' to Adjust Elasticity",10,100)
     text("'Z' or 'X' to Adjust Gravity",10,115)
 
-    text("Constants",10, 130)
+    text("Constants",10, 135)
     text("Size - " + size,10, 145)
-    text("Elasticity - " + (elasticity * 100) + "%", 10, 50)
-    text("Gravity - " + gravity,10,65)
+    text("Elasticity - " + (elasticity * 100) + "%", 10, 160)
+    text("Gravity - " + gravity,10, 175)
 
 
     // Sprite Creation
