36a6a2eb16275cb69247ed39924431ac1c85da40
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-05 08:06:22 +0000

    Oct 5, 2025, 8:06 AM

--- playgroundhtml/algoodo.js
+++ playgroundhtml/algoodo.js
@@ -18,7 +18,7 @@ function setup() {
 
     // Draw the right rectangle.
     Menu = new Sprite(width / 2, height / 2, width, height, "static");
-    Menu.color = color('#0f0');
+    Menu.color = color('rgba(0, 0, 0, 0.18)');
 
 }
 
