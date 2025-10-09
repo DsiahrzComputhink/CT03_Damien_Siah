38b9a9dbc3d5b74834a709766c38af4560fbd809
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-09 05:26:15 +0000

    Oct 9, 2025, 5:26 AM

--- playgroundhtml/algoodo.js
+++ playgroundhtml/algoodo.js
@@ -53,6 +53,7 @@ function draw() {
     text("Size - " + size,10, 180)
     text("Elasticity - " + Math.round(elasticity * 100) + "%", 10, 195)
     text("Gravity - " + gravity,10, 210)
+    text("Anchored - " + anchor,10, 210)
 
 
     // Sprite Creation
