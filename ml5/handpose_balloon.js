69741a87a30db62a17c5f6b6007d30e36cb05388
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-12 03:27:50 +0000

    Oct 12, 2025, 3:27 AM

--- ml5/handpose_balloon.js
+++ ml5/handpose_balloon.js
@@ -64,7 +64,7 @@ function setup(){
     // walls
     topWall = new Sprite(width /2, 0, width, 10, 'static');
     bottomWall = new Sprite(width/2, height, width, 10, 'static');
-    leftWall = new Sprite(0, height/2, 0, 10, 'static');
+    leftWall = new Sprite(0, height/2, 0, height, 'static');
 }
 
 function draw(){
