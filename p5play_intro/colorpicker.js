f33bf9a3a6cc49231cae21853c8e041b9b5b0fc2
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-12 04:17:18 +0000

    Oct 12, 2025, 4:17 AM

--- p5play_intro/colorpicker.js
+++ p5play_intro/colorpicker.js
@@ -4,9 +4,11 @@ let userText = "Your text here";
 function setup(){
     createCanvas(600,400);
     
-    // inputText = createInput();
-    // inputText.position(200,height - 80);
-    // inputText.input(updateText);
+    inputText = createInput();
+    inputText.position(200,height - 80);
+    inputText.input(updateText);
+
+    
 }
 
 function draw(){
@@ -16,11 +18,11 @@ function draw(){
     fill(0);
     text("Pick a Color",20, 50)
 
-    // background(220);
-    // fill(0);
-    // textSize(28);
-    // textAlign(CENTER, CENTER);
-    // text(userText, width/2, 180)
+    background(220);
+    fill(0);
+    textSize(28);
+    textAlign(CENTER, CENTER);
+    text(userText, width/2, 180)
 }
 
 function updateText(){
