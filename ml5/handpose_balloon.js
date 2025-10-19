20d23b2f5ccb31b3a93395e0f30296a9982dac7d
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-19 03:50:54 +0000

    Oct 19, 2025, 3:50 AM

--- ml5/handpose_balloon.js
+++ ml5/handpose_balloon.js
@@ -21,7 +21,7 @@ function preload(){
 
     // loading the handpose model
     handPose = ml5.handPose(options);
-    pointSound = createAudio("assets/sfx_point.mp3")
+    pointSound = createAudio("")
 }
 
 function setup(){
