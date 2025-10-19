a2ca6540841a932a34f62b251aefaa5330640ff3
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-19 03:24:28 +0000

    Oct 19, 2025, 3:24 AM

--- ml5/handpose_balloon.js
+++ ml5/handpose_balloon.js
@@ -94,6 +94,7 @@ function draw(){
     if (gameStarted === false) {
         textSize(28);
         textAlgn(CENTER, CENTER);
+        fill(black)
     }
     if (gameOver === false) {
         if (hands.length > 0) {
