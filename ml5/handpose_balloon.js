c03d66f64f3d3f8fc5c649ed9323d3f7b465a66a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-26 04:23:00 +0000

    Oct 26, 2025, 4:23 AM

--- ml5/handpose_balloon.js
+++ ml5/handpose_balloon.js
@@ -127,6 +127,7 @@ function draw(){
         textAlign(CENTER, CENTER);
         text("Game Over", width/2, height/2);
         textSize(18);
+        
     }
 
     // if (balloon.collides(fingerTip)){
