0ae1ad7c6375c480e9897454fd3a7097ac0aa4f3
DsiahrzComputhink <damiensiah11@gmail.com>
2025-11-02 04:16:18 +0000

    Nov 2, 2025, 4:16 AM

--- playgroundhtml/sample practical/challenge2.js
+++ playgroundhtml/sample practical/challenge2.js
@@ -11,7 +11,7 @@ function setup(){
     
     textSize(20);
     textAlign(CENTER, CENTER);
-    text("Enter a Noun");
+    text("Enter a Noun", width / 2 - );
 
     inputNoun = createInput();
     inputNoun.position(width / 2,height + 70);
